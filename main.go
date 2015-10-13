cb3c765e722aaa6dcad00d83fe29d5bf9b5a8c57
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2015-10-13 19:21:58 -0300

    changed the name of main file

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+
+	"github.com/RenatoAraujo/go-restful-application/controllers"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+
+	r.POST("/user", uc.CreateUser)
+
+	r.DELETE("/user/:id", uc.RemoveUser)
+
+	http.ListenAndServe("localhost:3000", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
